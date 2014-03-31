df54c359107b7fa8d71ca04c0455fe5226c8ed6f
zzltjnh <zzltjnh@gmail.com>
2014-03-31 09:22:22 +0800

    Initial Commit

---
+++ WeSee/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeSee
+//
+//  Created by zzl on 14-3-31.
+//  Copyright (c) 2014年 zzl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
